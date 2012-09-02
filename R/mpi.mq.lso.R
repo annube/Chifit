b08eb24f0133f39d55f78447c6fd7597d610d2a6
Andreas Nube <annube@ifh.de>
2012-09-02 16:39:05 +0200

    type removed ("error no symbol par.par")

--- R/mpi.mq.lso.R
+++ R/mpi.mq.lso.R
@@ -1,12 +1,12 @@
 
 
 mpi.mq.lso.cc <- function(x,par,aargs.par){
-  .Call("mpi_mq_lso",x,par.par,aargs.par,F,PACKAGE="chifit")
+  .Call("mpi_mq_lso",x,par,aargs.par,F,PACKAGE="chifit")
 }
 
 
 dmpi.mq.lso.cc <- function(x,par,aargs.par){
-  .Call("mpi_mq_lso",x,par.par,aargs.par,T,PACKAGE="chifit")
+  .Call("mpi_mq_lso",x,par,aargs.par,T,PACKAGE="chifit")
 }
 
 fpi.mq.lso.cc <- function(x,par,aargs.par){
