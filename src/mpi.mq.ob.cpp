92f0e4b371a1efc5fd6f1e6409cb40b91270818a
Andreas Nube <annube@ifh.de>
2012-11-22 13:49:49 +0100

    preparing mpi.mq.ob for implementation of functions where ZP is treated as additional fit parameter (not just an additional regressor)

--- src/mpi.mq.ob.cpp
+++ src/mpi.mq.ob.cpp
@@ -11,8 +11,9 @@ using namespace GiNaC;
 
 
 #include "gtilde.h"
-
 #include "eval.ex.lso.h"
+
+#include "mpi.mq.ob.h"
 
 
 /* we need a namespace here because the same symbols as below are defined in mpi.mq.cpp as well */
@@ -96,7 +97,7 @@ namespace mpi_mq_ob {
   /**************************
    *
    *
-   *        PION MASS
+   *    CHARGED PION MASS
    *
    *
    *
@@ -135,6 +136,14 @@ namespace mpi_mq_ob {
   }
 
 
+  /**************************
+   *
+   *
+   *    NEUTRAL PION MASS
+   *
+   *
+   *
+   **************************/
 
   RcppExport SEXP mpi_0_mq_ob(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
     static  ex mpisq = get_M_0_sq_Xpression();
@@ -169,6 +178,15 @@ namespace mpi_mq_ob {
 			  );
   }
 
+
+  /**************************
+   *
+   *
+   *    PION DECAY
+   *
+   *
+   *
+   **************************/
 
   RcppExport SEXP fpi_mq_ob(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
     static  ex fpisq = get_f_pm_Xpression();
---
+++ src/mpi.mq.ob.h
@@ -0,0 +1,15 @@
+
+
+
+
+/** 
+ * function definitions for obtaining the corresponding expressions 
+ */
+
+namespace mpi_mq_ob {
+
+static ex get_M_pm_sq_Xpression();
+static ex get_M_0_sq_Xpression();
+static ex get_f_pm_Xpression();
+
+};
