dcc25c9f68f4d361b206c281bdee2d8a9cd244c1
Andreas Nube <nubi@freenet.de>
2012-11-22 17:48:44 +0100

    corrected very misleading naming of parameters of the R wrapper functions  - x was exchanged with par, but as the function was passing it on also interchanged the order was correct at the end - but if one would read off the correct order of the parameters from the R-function one would produce wrong code

--- R/mpi.mq.ob.cc.R
+++ R/mpi.mq.ob.cc.R
@@ -1,31 +1,31 @@
 
 
-mpi.mq.ob.cc <- function(par,x,aargs){
-  .Call("mpi_mq_ob",par,x,aargs,F,PACKAGE="chifit")
+mpi.mq.ob.cc <- function(x,par,aargs){
+  .Call("mpi_mq_ob",x,par,aargs,F,PACKAGE="chifit")
 }
 
 
-dmpi.mq.ob.cc <- function(par,x,aargs){
-  .Call("mpi_mq_ob",par,x,aargs,T,PACKAGE="chifit")
+dmpi.mq.ob.cc <- function(x,par,aargs){
+  .Call("mpi_mq_ob",x,par,aargs,T,PACKAGE="chifit")
 }
 
-mpi.0.mq.ob.cc <- function(par,x,aargs){
-  .Call("mpi_0_mq_ob",par,x,aargs,F,PACKAGE="chifit")
-}
-
-
-dmpi.0.mq.ob.cc <- function(par,x,aargs){
-  .Call("mpi_0_mq_ob",par,x,aargs,T,PACKAGE="chifit")
+mpi.0.mq.ob.cc <- function(x,par,aargs){
+  .Call("mpi_0_mq_ob",x,par,aargs,F,PACKAGE="chifit")
 }
 
 
-fpi.mq.ob.cc <- function(par,x,aargs){
-  .Call("fpi_mq_ob",par,x,aargs,F,PACKAGE="chifit")
+dmpi.0.mq.ob.cc <- function(x,par,x,aargs){
+  .Call("mpi_0_mq_ob",x,par,aargs,T,PACKAGE="chifit")
 }
 
 
-dfpi.mq.ob.cc <- function(par,x,aargs){
-  .Call("fpi_mq_ob",par,x,aargs,T,PACKAGE="chifit")
+fpi.mq.ob.cc <- function(x,par,aargs){
+  .Call("fpi_mq_ob",x,par,aargs,F,PACKAGE="chifit")
+}
+
+
+dfpi.mq.ob.cc <- function(x,par,aargs){
+  .Call("fpi_mq_ob",x,par,aargs,T,PACKAGE="chifit")
 }
 
 
