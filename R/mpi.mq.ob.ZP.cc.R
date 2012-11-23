a990f3029a7e5061722540c25e8d69e10538f1ad
Andreas Nube <annube@ifh.de>
2012-11-23 14:09:28 +0100

    all ZP varying functions implemented

--- R/mpi.mq.ob.ZP.cc.R
+++ R/mpi.mq.ob.ZP.cc.R
@@ -9,24 +9,23 @@ dmpi.mq.ob.ZP.cc <- function(x,par,aargs){
   .Call("mpi_mq_ob_zp",x,par,aargs,T,PACKAGE="chifit")
 }
 
-## the "ZP"-versions of the following functions will be implemented in future
-## mpi.0.mq.ob.cc <- function(x,par,aargs){
-##   .Call("mpi_0_mq_ob",x,par,aargs,F,PACKAGE="chifit")
-## }
+mpi.0.mq.ob.ZP.cc <- function(x,par,aargs){
+  .Call("mpi_0_mq_ob_zp",x,par,aargs,F,PACKAGE="chifit")
+}
 
 
-## dmpi.0.mq.ob.cc <- function(x,par,x,aargs){
-##   .Call("mpi_0_mq_ob",x,par,aargs,T,PACKAGE="chifit")
-## }
+dmpi.0.mq.ob.ZP.cc <- function(x,par,aargs){
+  .Call("mpi_0_mq_ob_zp",x,par,aargs,T,PACKAGE="chifit")
+}
 
 
-## fpi.mq.ob.cc <- function(x,par,aargs){
-##   .Call("fpi_mq_ob",x,par,aargs,F,PACKAGE="chifit")
-## }
+fpi.mq.ob.ZP.cc <- function(x,par,aargs){
+  .Call("fpi_mq_ob_zp",x,par,aargs,F,PACKAGE="chifit")
+}
 
 
-## dfpi.mq.ob.cc <- function(x,par,aargs){
-##   .Call("fpi_mq_ob",x,par,aargs,T,PACKAGE="chifit")
-## }
+dfpi.mq.ob.ZP.cc <- function(x,par,aargs){
+  .Call("fpi_mq_ob_zp",x,par,aargs,T,PACKAGE="chifit")
+}
 
 
--- src/mpi.mq.ob.ZP.cpp
+++ src/mpi.mq.ob.ZP.cpp
@@ -19,20 +19,27 @@ using namespace std;
 namespace mpi_mq_ob{
 
 
-RcppExport SEXP mpi_mq_ob_zp(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
-  static  ex mpisq = get_M_pm_sq_Xpression();
+
+  /**
+   * chiPT description of pion mass 
+   *  - including isospin breaking parameter c2
+   *  - also variation w.r.t. ZP is calculated
+   */
+
+  RcppExport SEXP mpi_mq_ob_zp(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
+    static  ex mpisq = get_M_pm_sq_Xpression();
   
-  /* the main parameters to optimize for */
-  SymbolVec pureParVec;
-  vector<double> pureParDimE;
+    /* the main parameters to optimize for */
+    SymbolVec pureParVec;
+    vector<double> pureParDimE;
   
-  /* the parameters to be fitted and their energy dimension */
+    /* the parameters to be fitted and their energy dimension */
   
-  pureParVec.push_back(B);       pureParDimE.push_back(1.);
-  pureParVec.push_back(f);       pureParDimE.push_back(1.);
-  pureParVec.push_back(c2);      pureParDimE.push_back(4.);
-  pureParVec.push_back(Lambda3); pureParDimE.push_back(1.);
-  pureParVec.push_back(CMpm);    pureParDimE.push_back(2.);
+    pureParVec.push_back(B);       pureParDimE.push_back(1.);
+    pureParVec.push_back(f);       pureParDimE.push_back(1.);
+    pureParVec.push_back(c2);      pureParDimE.push_back(4.);
+    pureParVec.push_back(Lambda3); pureParDimE.push_back(1.);
+    pureParVec.push_back(CMpm);    pureParDimE.push_back(2.);
 
     
 
@@ -55,6 +62,102 @@ RcppExport SEXP mpi_mq_ob_zp(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
 			  lsDepPar
 			  );
   }
+
+
+
+  /**************************
+   *
+   *
+   *    PION DECAY
+   *
+   *
+   *
+   **************************/
+
+  RcppExport SEXP fpi_mq_ob_zp(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
+    static  ex fpisq = get_f_pm_Xpression();
+
+    /* the main parameters to optimize for */
+    SymbolVec pureParVec;
+    vector<double> pureParDimE;
+
+    /* the parameters to be fitted and their energy dimension */
+
+    pureParVec.push_back(B);       pureParDimE.push_back(1.);
+    pureParVec.push_back(f);       pureParDimE.push_back(1.);
+    pureParVec.push_back(c2);      pureParDimE.push_back(4.);
+    pureParVec.push_back(Lambda4); pureParDimE.push_back(1.);
+    pureParVec.push_back(Cf);      pureParDimE.push_back(2.);
+
+    
+
+
+    /* additional regressors besides the main regressor */
+    SymbolStringVec ssvec;
+    ssvec.push_back( SymbolStringPair( &L , "L" ) );
+
+
+    SymbolVec lsDepPar;
+    lsDepPar.push_back(ZP);
+
+    return    eval_ex_lso(fpisq, /* the expression to work on */
+			  x,par,aargs,deri, /* pass on the parameters from R environment */
+			  mu, /* the main regressor appearing in the expression */
+			  pureParVec,  /* a vector of parameters to optimize for */
+			  pureParDimE,
+			  ssvec,   /* a vector of additional regresssor and their name in the aargs list */
+			  lsDepPar
+			  );
+  }
+
+
+
+
+  /**************************
+   *
+   *
+   *    NEUTRAL PION MASS
+   *
+   *
+   *
+   **************************/
+
+  RcppExport SEXP mpi_0_mq_ob_zp(SEXP x, SEXP par,SEXP aargs,SEXP deri) {
+    static  ex mpisq = get_M_0_sq_Xpression();
+
+    /* the main parameters to optimize for */
+    SymbolVec pureParVec;
+    vector<double> pureParDimE;
+
+    /* the parameters to be fitted and their energy dimension */
+
+    pureParVec.push_back(B);       pureParDimE.push_back(1.);
+    pureParVec.push_back(f);       pureParDimE.push_back(1.);
+    pureParVec.push_back(c2);      pureParDimE.push_back(4.);
+    pureParVec.push_back(Lambda3); pureParDimE.push_back(1.);
+    pureParVec.push_back(Xi3);     pureParDimE.push_back(1.);
+    pureParVec.push_back(CM0);    pureParDimE.push_back(2.);
+
+    
+
+
+    /* additional regressors besides the main regressor */
+    SymbolStringVec ssvec;
+    ssvec.push_back( SymbolStringPair( &L , "L" ) );
+
+    SymbolVec lsDepPar;
+    lsDepPar.push_back(ZP);
+
+    return    eval_ex_lso(mpisq, /* the expression to work on */
+			  x,par,aargs,deri, /* pass on the parameters from R environment */
+			  mu, /* the main regressor appearing in the expression */
+			  pureParVec,  /* a vector of parameters to optimize for */
+			  pureParDimE,
+			  ssvec,  /* a vector of additional regresssor and their name in the aargs list */
+			  lsDepPar
+			  );
+  }
+
 
 
 };
