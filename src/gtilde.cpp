7308ad681751b966b998d09609d4a7be8e58795c
Andreas Nube <annube@ifh.de>
2012-11-28 15:44:59 +0100

    code nicifzy

--- src/gtilde.cpp
+++ src/gtilde.cpp
@@ -46,7 +46,7 @@ double eval_dgtilde1(double x){
       4. * (double) getPartition(i) / ( sx ) *(
 					       - 1. / x *  bessel_k( sx , 1. , 1.) 
 					       -0.5 * sqrt( (double)i ) * ( bessel_k( sx , 0. , 1.)  + bessel_k( sx , 2. , 1.) ) );
-;
+    ;
     sum += ds ; 
     //    cout << abs(ds/sum) << endl;
   }
@@ -77,52 +77,56 @@ RcppExport SEXP eval_dgtilde1_R( SEXP x){
   return vx;
 }
 
+
+
+// functions for ginac
+
 ex g_tilde_1_deri_eval(const ex &x){
   return dgtilde1(x).hold();
 }
 
 
- ex g_tilde_1_deri_evalf(const ex &x){
+ex g_tilde_1_deri_evalf(const ex &x){
   if( is_a<numeric>(x) ) {
     return eval_dgtilde1(ex_to<numeric>(x).to_double()) ;
   } else {
-     return dgtilde1(x).hold();
+    return dgtilde1(x).hold();
   }
 }
 
 
- ex g_tilde_1_deri_eval_deri(const ex &x,unsigned diffpar ){
+ex g_tilde_1_deri_eval_deri(const ex &x,unsigned diffpar ){
   return 1./0.;
 }
 
 
- void g_tilde_1_deri_print(const ex & arg, const print_context & c)
-     {
-         c.s << "gtilde_deri("; arg.print(c); c.s << ")";
-     }
+void g_tilde_1_deri_print(const ex & arg, const print_context & c)
+{
+  c.s << "gtilde_deri("; arg.print(c); c.s << ")";
+}
 
 
 
 
 
- ex g_tilde_1_eval(const ex &x){
+ex g_tilde_1_eval(const ex &x){
   return gtilde1(x).hold();
 }
 
 
- ex g_tilde_1_evalf(const ex &x){
+ex g_tilde_1_evalf(const ex &x){
   if( is_a<numeric>(x) ) {
     return eval_gtilde1(ex_to<numeric>(x).to_double()) ;
   } else {
-     return gtilde1(x).hold();
+    return gtilde1(x).hold();
   }
 }
 
 
- ex g_tilde_1_eval_deri(const ex &x,unsigned diffpar ){
-   //  cout << "called g_tilde_1_eval_deri" << endl;
-     return dgtilde1(x);
- }
+ex g_tilde_1_eval_deri(const ex &x,unsigned diffpar ){
+  //  cout << "called g_tilde_1_eval_deri" << endl;
+  return dgtilde1(x);
+}
 
 
 
@@ -130,8 +134,8 @@ REGISTER_FUNCTION(dgtilde1,
 		  eval_func(g_tilde_1_deri_eval).
 		  evalf_func(g_tilde_1_deri_evalf)//.
 		  //		  derivative_func(g_tilde_1_deri_eval_deri).
-// 		  latex_name("\\tilde{g}_1'").
-// 		  print_func<print_dflt>(g_tilde_1_deri_print)
+		  // 		  latex_name("\\tilde{g}_1'").
+		  // 		  print_func<print_dflt>(g_tilde_1_deri_print)
 		  );
 
 REGISTER_FUNCTION(gtilde1, 
