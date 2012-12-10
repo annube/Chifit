5f073d2bb05b39bd1a2db2eef1235f4fc3182628
Andreas Nube <annube@ifh.de>
2012-12-10 10:09:35 +0100

    unecesarry comments removed in mpi.mq.ob.cpp

--- src/mpi.mq.ob.cpp
+++ src/mpi.mq.ob.cpp
@@ -15,6 +15,7 @@ using namespace GiNaC;
 
 #include "symbols.h"
 #include "mpi.mq.ob.h"
+#include "mpi.mq.gen.h"
 #include "tmFS.h"
 
 
@@ -23,18 +24,6 @@ using namespace GiNaC;
 
 /* we need a namespace here because the same symbols as below are defined in mpi.mq.cpp as well */
 namespace chifit {
-
-// class Mpm_sq_ex{public: 
-//     Mpm_sq_ex(){} 
-//   operator GiNaC::ex() const {return 2*B*mu/ZP;}
-// };
-
-// class M0_sq_ex{public: 
-//     M0_sq_ex(){} 
-//   operator GiNaC::ex() const {return sqrt( pow(  2*B*mu/ZP + 2*c2 ,2 ) );}
-// };
-
-
 
   Mpm_sq_ex Mpm_sq;
   M0_sq_ex M0_sq; 
@@ -134,53 +123,9 @@ namespace chifit {
    *
    **************************/
 
+
   RcppExport SEXP mpi_mq_ob(SEXP x, SEXP par,SEXP aargs,SEXP deri,SEXP FSE,SEXP fitZP) {
-
-    ParameterMap pm;
-
-    ex mpisq = get_M_pm_sq_Xpression(pm);
-
-    //    pm.print();
-
-    /* the main parameters to optimize for */
-    SymbolVec pureParVec;
-    vector<double> pureParDimE;
-
-    /* the parameters to be fitted and their energy dimension */
-    /* they will be autagically selected by the expression generating function */
-
-    const SymbolBoolVec & useMap=pm.getMap();
-
-
-    for( int p  = 0 ; p < useMap.size() ; p++){
-      if( useMap[p].second ) { 
-	pureParVec.push_back( useMap[p].first );
-	pureParDimE.push_back( allSymbolsOrderedDimensions[p] );
-      }
-    }
-    
-
-
-
-
-    /* additional regressors besides the main regressor */
-    SymbolStringVec ssvec;
-    SymbolVec lsDepPar;
-    ssvec.push_back( SymbolStringPair( &L , "L" ) );
-
-    if( ! Rcpp::as<bool>(fitZP) )
-      ssvec.push_back( SymbolStringPair( &ZP , "ZP" ) );
-    else      
-      lsDepPar.push_back(ZP);
-
-
-    return    eval_ex_lso(mpisq, /* the expression to work on */
-			  x,par,aargs,deri, /* pass on the parameters from R environment */
-			  mu, /* the main regressor appearing in the expression */
-			  pureParVec,  /* a vector of parameters to optimize for */
-			  pureParDimE,
-			  ssvec,   /* a vector of additional regresssor and their name in the aargs list */
-			  lsDepPar);
+    return mpi_mq_gen(&get_M_pm_sq_Xpression,x,par,aargs,deri,FSE,fitZP);
   }
 
 
@@ -194,48 +139,7 @@ namespace chifit {
    **************************/
 
   RcppExport SEXP mpi_0_mq_ob(SEXP x, SEXP par,SEXP aargs,SEXP deri,SEXP FSE, SEXP fitZP) {
-    ParameterMap pm;
-    ex mpisq = get_M_0_sq_Xpression(pm);
-
-    /* the main parameters to optimize for */
-    SymbolVec pureParVec;
-    vector<double> pureParDimE;
-
-    /* the parameters to be fitted and their energy dimension */
-    /* they will be autagically selected by the expression generating function */
-
-    const SymbolBoolVec & useMap=pm.getMap();
-
-
-    for( int p  = 0 ; p < useMap.size() ; p++){
-      if( useMap[p].second ) { 
-	pureParVec.push_back( useMap[p].first );
-	pureParDimE.push_back( allSymbolsOrderedDimensions[p] );
-      }
-    }
-
-    
-
-
-    /* additional regressors besides the main regressor */
-    SymbolStringVec ssvec;
-    SymbolVec lsDepPar;
-    ssvec.push_back( SymbolStringPair( &L , "L" ) );
-
-    if( ! Rcpp::as<bool>(fitZP) )
-      ssvec.push_back( SymbolStringPair( &ZP , "ZP" ) );
-    else      
-      lsDepPar.push_back(ZP);
-
-
-    return    eval_ex_lso(mpisq, /* the expression to work on */
-			  x,par,aargs,deri, /* pass on the parameters from R environment */
-			  mu, /* the main regressor appearing in the expression */
-			  pureParVec,  /* a vector of parameters to optimize for */
-			  pureParDimE,
-			  ssvec,   /* a vector of additional regresssor and their name in the aargs list */
-			  lsDepPar
-			  );
+    return mpi_mq_gen(&get_M_0_sq_Xpression,x,par,aargs,deri,FSE,fitZP);
   }
 
 
@@ -249,45 +153,7 @@ namespace chifit {
    **************************/
 
   RcppExport SEXP fpi_mq_ob(SEXP x, SEXP par,SEXP aargs,SEXP deri, SEXP FSE, SEXP fitZP) {
-    ParameterMap pm;
-    ex fpisq = get_f_pm_Xpression(pm);
-
-    /* the main parameters to optimize for */
-    SymbolVec pureParVec;
-    vector<double> pureParDimE;
-
-    /* the parameters to be fitted and their energy dimension */
-
-    const SymbolBoolVec & useMap=pm.getMap();
-
-
-    for( int p  = 0 ; p < useMap.size() ; p++){
-      if( useMap[p].second ) { 
-	pureParVec.push_back( useMap[p].first );
-	pureParDimE.push_back( allSymbolsOrderedDimensions[p] );
-      }
-    }
-
-
-
-    /* additional regressors besides the main regressor */
-    SymbolStringVec ssvec;
-    SymbolVec lsDepPar;
-    ssvec.push_back( SymbolStringPair( &L , "L" ) );
-
-    if( ! Rcpp::as<bool>(fitZP) )
-      ssvec.push_back( SymbolStringPair( &ZP , "ZP" ) );
-    else      
-      lsDepPar.push_back(ZP);
-
-    return    eval_ex_lso(fpisq, /* the expression to work on */
-			  x,par,aargs,deri, /* pass on the parameters from R environment */
-			  mu, /* the main regressor appearing in the expression */
-			  pureParVec,  /* a vector of parameters to optimize for */
-			  pureParDimE,
-			  ssvec,   /* a vector of additional regresssor and their name in the aargs list */
-			  lsDepPar
-			  );
+    return mpi_mq_gen(&get_f_pm_Xpression,x,par,aargs,deri,FSE,fitZP);
   }
 
 
