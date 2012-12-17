1be400fe63ceee9cf31c519c37f2dc3da60afcf8
Andreas Nube <annube@ifh.de>
2012-12-17 11:15:00 +0100

    basic class layout of ExpressionFactory Rcpp function for generating expressions and getting corresponding ids

--- src/ExpressionFactory.cpp
+++ src/ExpressionFactory.cpp
@@ -1,11 +1,55 @@
 
+#include <iostream>
+
+using namespace std;
+
+
+#include <ginac/ginac.h>
+
+using namespace GiNaC;
+
+#include <Rcpp.h>
+
+using namespace Rcpp;
 
 
 #include "ExpressionFactory.h"
 
-
 namespace chifit {
 
+
   
+  ExpressionFactory *ExpressionFactory::instance = NULL;
+
+
+  ExpressionFactory::ExpressionFactory():nextId(0){}
+
+
+  ExpressionFactory *ExpressionFactory::getInstance(){
+    if( ExpressionFactory::instance == NULL ){
+      cout << "Generating new ExpressionFactory instance .." << endl;
+      ExpressionFactory::instance = new ExpressionFactory;
+    }
+    return ExpressionFactory::instance;
+  }
+
+  int ExpressionFactory::generateXPression(const string &quant,const string &FSE){
+    return -1;
+  }
+
+
+
+
+  RcppExport SEXP GenerateChifitExpression(SEXP quantity,SEXP FSE){
+    return wrap(
+		ExpressionFactory::getInstance()->
+		generateXPression(
+				  as<string>(quantity),
+				  as<string>(FSE) 
+				  )
+		);
+  }
+
+
 
 };
--- src/ExpressionFactory.h
+++ src/ExpressionFactory.h
@@ -4,13 +4,75 @@
 #ifndef EXPRESSION_FACTORY
 #define EXPRESSION_FACTORY
 
+#include <map>
+#include <string>
+
+using namespace std;
+
+#include <ginac/ginac.h>
+
+using namespace GiNaC;
+
+#include <Rcpp.h>
+
+using namespace Rcpp;
 
 
 namespace chifit {
 
+
+  typedef map<int,ex> ExFromIdMap;
+
   class ExpressionFactory {
 
+  private:
+
+    /**
+     * instance to ExpressionFactory
+     */
+    static ExpressionFactory *instance;
+
+    /**
+     * a map storing all expressions by id
+     */
+    ExFromIdMap XMap;
+
+    /**
+     * the next id to generate
+     */
+    int nextId;
+
+
+    /**
+     * return the next id to generate
+     * use only this function to access nextId
+     * accept the constructor (where nextId is only initialized)
+     */
+    int getNextId(){ return nextId++; }
+
+
+    /**
+     * constructor
+     */
+    ExpressionFactory();
+
+  public:
+
+    static ExpressionFactory * getInstance();
+
+    /**
+     * generate expressions
+     */
+    int generateXPression(const string &quant,const string &FSE);
+
+
   };
+
+
+  /**
+   * Rcpp wrapper function for generating expressions
+   */
+  RcppExport SEXP GenerateChifitExpression(SEXP quantity,SEXP FSE);
 
 };
 
