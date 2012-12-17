23bc4135d2b57940b63f70768734657ba2a3bb62
Andreas Nube <annube@ifh.de>
2012-12-17 11:45:25 +0100

    new R function for the evaluation of an expression generated by the Factory

--- src/mpi.mq.gen.cpp
+++ src/mpi.mq.gen.cpp
@@ -71,4 +71,56 @@ namespace chifit {
 			  lsDepPar);
   }
 
+  SEXP mpi_mq_gen_id(SEXP id,SEXP x, SEXP par,SEXP aargs,SEXP deri,SEXP fitZP) {
+
+    ParameterMap pm;
+
+    GiNaC::ex mpisq;
+
+//     mpisq = ExpressionFactory::getInstance()->getExpression(as<int>(id));
+//     pm = ExpressionFactory::getInstance()->getExpression(as<int>(id));
+
+
+    /* the main parameters to optimize for */
+    SymbolVec pureParVec;
+    vector<double> pureParDimE;
+
+    /* the parameters to be fitted and their energy dimension */
+    /* they will be autagically selected by the expression generating function */
+
+    const SymbolBoolVec & useMap=pm.getMap();
+
+
+    for( int p  = 0 ; p < useMap.size() ; p++){
+      if( useMap[p].second ) { 
+	//	cout <<  useMap[p].first << endl ;
+	pureParVec.push_back( useMap[p].first );
+	pureParDimE.push_back( allSymbolsOrderedDimensions[p] );
+      }
+    }
+    
+
+
+
+
+    /* additional regressors besides the main regressor */
+    SymbolStringVec ssvec;
+    SymbolVec lsDepPar;
+    ssvec.push_back( SymbolStringPair( &L , "L" ) );
+
+    if( ! Rcpp::as<bool>(fitZP) )
+      ssvec.push_back( SymbolStringPair( &ZP , "ZP" ) );
+    else      
+      lsDepPar.push_back(ZP);
+
+
+    return    eval_ex_lso(mpisq, /* the expression to work on */
+			  x,par,aargs,deri, /* pass on the parameters from R environment */
+			  mu, /* the main regressor appearing in the expression */
+			  pureParVec,  /* a vector of parameters to optimize for */
+			  pureParDimE,
+			  ssvec,   /* a vector of additional regresssor and their name in the aargs list */
+			  lsDepPar);
+  }
+
 };
