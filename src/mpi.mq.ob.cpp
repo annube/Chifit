cad85241e5f766030cf09aeed81d40a8f0153e34
Andreas Nube <annube@ifh.de>
2013-02-16 11:24:48 +0100

    critical bug in fit formula

--- src/mpi.mq.ob.cpp
+++ src/mpi.mq.ob.cpp
@@ -65,7 +65,7 @@ namespace chifit {
 
     static ex log_M0_X3 = log( M0_sq / pow( Xi3 , 2 )  )  ;
 
-    static ex X_FSE =   Mpm_sq * ( 1. +2.*xi_pm * ( log_Mpm_L3 + fse_log_corr_Mpm ) - xi_0 * ( log_M0_L3 +  + fse_log_corr_M0 )) 
+    static ex X_FSE =   Mpm_sq * ( 1. +2.*xi_pm * ( log_Mpm_L3 + fse_log_corr_Mpm ) - xi_0 * ( log_M0_L3 + fse_log_corr_M0 )) 
       + 2 * c2 *( 1 - 4. * xi_0 * ( log_M0_X3 + fse_log_corr_M0) + CM0); 
 
     static ex X =   Mpm_sq * ( 1. +2.*xi_pm * log_Mpm_L3 - xi_0 * log_M0_L3 ) 
