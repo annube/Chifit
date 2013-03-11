0b2ef6511f79a2fe2e1c7755a33730b1b2cf2c0d
Andreas Nube <annube@ifh.de>
2013-03-11 14:42:12 +0100

    also inverse function for make_hr_quantity implemented

--- R/make_hr_quantity.R
+++ R/make_hr_quantity.R
@@ -27,3 +27,45 @@ make_hr_quantity <- function(q,dq,min_err=5,num_digits) {
 
   return( sprintf(format,q,round(dq*10^num_digits)) )
 }
+
+
+## the inverse function
+
+convert.from.hr.quantity <- function(string,expected.len=1) {
+  ## remove whitespace
+  string <-  gsub(" " , "" , string)
+
+  ## split away different parts
+  parts <- unlist( strsplit(string,"[()]") )
+
+  non.zero.len.range <- which( sapply(parts,nchar) != 0 )
+
+  parts = parts[non.zero.len.range]
+
+  
+  
+  print( parts )
+  ## determine number of digits after the comma
+
+  after.comma.string <- unlist ( strsplit( parts[1] , split = "[.]" ) ) [2]
+  digits <- length( unlist( strsplit(after.comma.string,split="") ) )
+
+  value <- try( as.numeric( parts[1] ) ,TRUE)
+
+  res <- numeric(length(parts))
+  res[1] = value
+
+  if( length(parts) > 1 )
+    for( i in 2:length(parts) ){
+      digits.err <- length( unlist( strsplit(parts[i],split="") ) )
+      error.str <- paste0( c( "0." , rep("0" , digits-digits.err ) , parts[i]) , collapse="" )
+      print(error.str)
+      res[i] = as.numeric(error.str)
+    }
+
+  if( is.na( res[1] ) )
+    return( rep( NA ,expected.len ) )
+  else 
+    return( res )
+  
+}                             
